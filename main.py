002e519203c576dbec52ae2255048b5ca8079431
Tilman Stehr <tilman@tilman.ninja>
2018-02-22 22:20:45 +0100

    Fix defaults being defined in the wrong file

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import actions
 from sList import List
 from config import config,args
 
+DEFAULT_SERVER = 'https://list.tilman.ninja'
+DEFAULT_LIST   = 'Demo'
 
 sl=List(config.get('server') or DEFAULT_SERVER,config.get('list') or DEFAULT_LIST)
 
